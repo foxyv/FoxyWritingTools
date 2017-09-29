1f58110dc54eebb5543a84e2cb21e6f1191213ab
Verna Smith <dutyandcourage@gmail.com>
2017-09-29 16:50:14 -0700

    Initial commit

---
+++ src/main/java/ninja/foxyv/writing/Main.java
@@ -0,0 +1,18 @@
+package ninja.foxyv.writing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by verna on 9/29/17.
+ */
+@SpringBootApplication
+public class Main {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
